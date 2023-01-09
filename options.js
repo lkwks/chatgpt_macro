0b5ded43a9e4e3b1aee64bf6f93a92fe59ca4000
Kangwook Lee <lkwks@naver.com>
2023-01-09 21:23:34 +0900

    Update options.js

--- options.js
+++ options.js
@@ -5,15 +5,19 @@ function save(key, value) {
 
 function render_saved_text(saved_text_dict, selected_text)
 {
-    var select_obj = document.querySelector("#saved_text select");
+    var saved_obj = document.querySelector("#saved_text");
+    var select_obj = saved_obj.querySelector("select");
+    var textarea_obj = saved_obj.querySelector("textarea");
+    var input_obj = saved_obj.querySelector("div.add_text_name input");
     var start_obj = document.querySelector("#start_with select");
     var box_obj = document.querySelector("#start_with item div:nth-child(2)");
+    
     if (saved_text_dict)
     {
         for (const key of Object.keys(saved_text_dict))
         {
             if (key === "-1") continue;
-            if (document.querySelector("#saved_text textarea").value === "") document.querySelector("#saved_text textarea").value = saved_text_dict[key];
+            if (textarea_obj.value === "") textarea_obj.value = saved_text_dict[key];
             var opt = document.createElement("option");
             opt.text = key;
             opt.value = key;
@@ -36,10 +40,22 @@ function render_saved_text(saved_text_dict, selected_text)
     }
 
     select_obj.addEventListener("change", ()=>{
-        document.querySelector("#saved_text textarea").value = saved_text_dict[select_obj.value];
+        textarea_obj.value = saved_text_dict[select_obj.value];
     });
+
     if(select_obj.querySelector("option") && window.location.href.includes("?opt="))
-        select_obj.querySelector(`option[value='${decodeURI(window.location.href).split("?opt=")[1]}']`).click();
+    {
+        var opt = decodeURI(window.location.href).split("?opt=")[1], sel_id = null;
+        select_obj.querySelectorAll("option").forEach((elem, i) => {
+            if (elem.value === opt)
+                sel_id = i;
+        });
+        if (sel_id)
+        {
+            select_obj.selectedIndex = sel_id;
+            textarea_obj.value = saved_text_dict[opt];
+        }
+    }
 
     start_obj.addEventListener("change", ()=>{
         if (selected_text.has(start_obj.value) === false && start_obj.value !== "-1")
@@ -65,6 +81,38 @@ function render_saved_text(saved_text_dict, selected_text)
         }
       });    
 
+    saved_obj.querySelector("button.save_textarea").addEventListener("click", ()=>{
+        if (select_obj.querySelector("option"))
+            saved_text[select_obj.value] = textarea_obj.value;
+        save("saved_text", JSON.stringify(saved_text));
+    });
+
+    saved_obj.querySelector("button.new_text").addEventListener("click", ()=>{
+        add_text_name_mode = "new";
+        input_obj.value = '';
+        saved_obj.querySelector("div.add_text_name").classList.toggle("hide");
+    });
+
+    saved_obj.querySelector("button.new_text").click();
+
+    saved_obj.querySelector("button.mod_text").addEventListener("click", ()=>{
+        add_text_name_mode = "mod";
+        input_obj.value = select_obj.value;
+        saved_obj.querySelector("div.add_text_name").classList.toggle("hide");
+    });
+
+    saved_obj.querySelector("button.del_text").addEventListener("click", ()=>{
+        if (select_obj.querySelector("option"))
+        {
+            delete saved_text[select_obj.value];
+            save("saved_text", JSON.stringify(saved_text));
+            window.location.href = window.location.href.split("?")[0];
+        }
+    });
+
+    saved_obj.querySelector("button.save_text_name").addEventListener("click", ()=>{save_text_name_func(add_text_name_mode);});
+    input_obj.addEventListener("keydown", (e)=>{if (e.key === "Enter")save_text_name_func(add_text_name_mode);});
+
 }
 
 let saved_text = {"-1":""}, add_text_name_mode = "", selected_text = new Set();
@@ -73,6 +121,7 @@ function save_text_name_func(mode)
 {
     var saved_text_obj = document.querySelector("#saved_text"); 
     var new_text_name = saved_text_obj.querySelector("div.add_text_name input").value.trim();
+
     if (new_text_name !== "")
     {
         if (mode === "new")
@@ -86,79 +135,72 @@ function save_text_name_func(mode)
         }
         else
         {
-            var temp = saved_text[saved_text_obj.querySelector("select").value];
-            delete saved_text[saved_text_obj.querySelector("select").value];
+            var old_val = saved_text_obj.querySelector("select").value;
+            var temp = saved_text[old_val];
+            delete saved_text[old_val];
             saved_text[new_text_name] = temp;
             var select_obj = saved_text_obj.querySelector(`select option[value='${saved_text_obj.querySelector('select').value}']`);
             select_obj.text = new_text_name;
             select_obj.value = new_text_name;
             save("saved_text", JSON.stringify(saved_text));
+
+            selected_text.delete(old_val);
+            selected_text.add(new_text_name);
+            save("selected_text", JSON.stringify(Array.from(selected_text)));
+
+            chrome.storage.sync.get(["text_used_num"], (items) => {
+                if (items.text_used_num)
+                {
+                    var used_num = JSON.parse(items.text_used_num);
+                    var temp = used_num[old_val];
+                    delete used_num[old_val];
+                    used_num[new_text_name] = temp;
+                    save("text_used_num", JSON.stringify(used_num));
+                }
+            });
+
         }
-        saved_text_obj.querySelector(`select option[value='${new_text_name}']`).click();
-        saved_text_obj.querySelector("div.add_text_name input").value = '';
         window.location.href = encodeURI(`${window.location.href}?opt=${new_text_name}`);
     }    
 }
 
+
+async function get_saved_text()
+{
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get(null, (items) => {
+            console.log(items);
+            if (items.api_key)
+                document.querySelector("#api_key input").value = items.api_key;
+           
+            if (items.selected_text)
+            try{
+                selected_text = new Set(JSON.parse(items.selected_text));
+            }
+            catch {
+                selected_text = [];
+                chrome.storage.sync.set({"selected_text": JSON.stringify(selected_text)});
+            }
+
+            if (items.tts_generating && items.tts_generating === "true")
+                document.querySelector("#tts_generating input").checked = true;
+
+            if (items.saved_text)
+                for (const [key, val] of Object.entries(JSON.parse(items.saved_text)))
+                    saved_text[key] = val;
+            document.querySelector("#restore_backup textarea").value = JSON.stringify(items);
+            resolve([saved_text, selected_text]);
+        });
+    });   
+}
+
+
 async function restoreOptions() {
-    const get_saved_text = ()=> {
-        return new Promise((resolve, reject) => {
-            chrome.storage.sync.get(null, (items) => {
-                if (items.api_key)
-                    document.querySelector("#api_key input").value = items.api_key;
-               
-                if (items.selected_text)
-                    selected_text = new Set(JSON.parse(items.selected_text));
-
-                if (items.tts_generating && items.tts_generating === "true")
-                    document.querySelector("#tts_generating input").checked = true;
-
-                if (items.saved_text)
-                    for (const [key, val] of Object.entries(JSON.parse(items.saved_text)))
-                        saved_text[key] = val;
-                document.querySelector("#restore_backup textarea").value = JSON.stringify(items);
-                resolve([saved_text, selected_text]);
-            });
-        });   
-    };
-    [saved_text, selected_text] = await get_saved_text();
-    render_saved_text(saved_text, selected_text);
+    render_saved_text(...(await get_saved_text()));
 
     document.querySelector("#api_key button").addEventListener("click", ()=>{
         save("api_key", document.querySelector("#api_key input").value);
     });
-
-    document.querySelector("#saved_text button.save_textarea").addEventListener("click", ()=>{
-        var select_obj = document.querySelector("#saved_text select");
-        if (select_obj.querySelector("option"))
-            saved_text[select_obj.value] = document.querySelector("#saved_text textarea").value;
-        save("saved_text", JSON.stringify(saved_text));
-    });
-
-    document.querySelector("#saved_text button.new_text").addEventListener("click", ()=>{
-        add_text_name_mode = "new";
-        document.querySelector("#saved_text div.add_text_name").classList.toggle("hide");
-    });
-    document.querySelector("#saved_text button.new_text").click();
-
-    document.querySelector("#saved_text button.mod_text").addEventListener("click", ()=>{
-        add_text_name_mode = "mod";
-        document.querySelector("#saved_text div.add_text_name input").value = document.querySelector("#saved_text select").value;
-        document.querySelector("#saved_text div.add_text_name").classList.remove("hide");
-    });
-
-    document.querySelector("#saved_text button.del_text").addEventListener("click", ()=>{
-        var select_obj = document.querySelector("#saved_text select");
-        if (select_obj.querySelector("option"))
-        {
-            delete saved_text[select_obj.value];
-            save("saved_text", JSON.stringify(saved_text));
-            window.location.href = window.location.href.split("?")[0];
-        }
-    });
-
-    document.querySelector("#saved_text button.save_text_name").addEventListener("click", ()=>{save_text_name_func(add_text_name_mode);});
-    document.querySelector("#saved_text div.add_text_name input").addEventListener("keydown", (e)=>{if (e.key === "Enter")save_text_name_func(add_text_name_mode);});
 
 
     document.querySelector("#restore_backup button").addEventListener("click", ()=>
